d9be1407e622037265398e8cd9dcc461e3fc875e
Filip Melka <melka.filip@proton.me>
2025-09-05 15:50:53 +0200

    feat: add publish date

---
+++ packages/app/scripts/add-pub-date.js
@@ -0,0 +1,27 @@
+import matter from 'gray-matter'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { glob } from 'glob'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const mdxFiles = await glob(path.join(__dirname, '../src/blog/**/*.mdx'))
+
+mdxFiles.forEach((filePath) => addPubDate(filePath))
+
+function addPubDate(path) {
+  const file = fs.readFileSync(path, 'utf-8')
+  const { data, content } = matter(file)
+
+  if (!data.pubDate) {
+    const today = new Date().toISOString().split('T')[0]
+    data.pubDate = today
+
+    const newFile = matter.stringify(content, data)
+
+    fs.writeFileSync(path, newFile)
+  }
+}
