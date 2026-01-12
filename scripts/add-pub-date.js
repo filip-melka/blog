dd15661a0971f2fe7cd41ad4f16bebbd29580322
Filip Melka <melka.filip@proton.me>
2026-01-12 16:33:28 +0100

    feat: add script to automatically add publication date to MDX files

---
+++ scripts/add-pub-date.js
@@ -0,0 +1,31 @@
+import matter from "gray-matter"
+import fs from "fs"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+import { glob } from "glob"
+import path from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const mdxFiles = await glob(path.join(__dirname, "../src/blog/**/*.mdx"))
+
+mdxFiles.forEach((filePath) => addPubDate(filePath))
+
+function addPubDate(path) {
+  const file = fs.readFileSync(path, "utf-8")
+  const { data, content } = matter(file)
+
+  if (!data.pubDate && !data.isDraft) {
+    const today = new Date()
+    today.setUTCHours(0)
+    today.setMinutes(0)
+    today.setSeconds(0)
+    today.setMilliseconds(0)
+    data.pubDate = today
+
+    const newFile = matter.stringify(content, data)
+
+    fs.writeFileSync(path, newFile)
+  }
+}
