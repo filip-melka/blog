49ff465d759d100151f599460957a1bf6f93bf5a
Filip Melka <melka.filip@proton.me>
2026-01-13 17:23:06 +0100

    fix: correct class names and structure in ScrollUpBtn component

--- src/components/react/scroll-up-btn.tsx
+++ src/components/react/scroll-up-btn.tsx
@@ -2,8 +2,11 @@ import { ChevronUp } from "lucide-react"
 
 export function ScrollUpBtn() {
   return (
-    <a href="#" className="cursor-pointe sticky bottom-20 z-10 block w-fit">
-      <button className="cursor-pointer rounded-full">
+    <a
+      href="#"
+      className="sticky bottom-20 z-10 inline-flex h-fit w-fit items-center justify-center"
+    >
+      <button className="cursor-pointer rounded-full p-0">
         <ChevronUp
           className="text-text scale-75 transition-transform hover:scale-100"
           size={50}
