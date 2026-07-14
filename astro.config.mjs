2c435609ea30fe0a791029bc57a709cd2d490ea9
Filip Melka <melka.filip@proton.me>
2026-07-14 12:55:13 +0200

    feat: add reading time badge

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import mdx from '@astrojs/mdx';
+import mdx from '@astrojs/mdx'
+import { unified } from '@astrojs/markdown-remark'
+import { remarkReadingTime } from './src/plugins/remark/reading-time.ts'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkReadingTime],
+    }),
   },
 
   integrations: [mdx()],
