a2af1ac5bf4f38b0e33b435e5b356b121dc8a628
Filip Melka <melka.filip@proton.me>
2026-08-02 20:01:26 +0200

    refactor: move article routes from /blog/articles/[slug] to /blog/[slug]

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       title: article.data.title,
       pubDate: article.data.pubDate,
       description: article.data.description,
-      link: `/blog/articles/${article.id}/`,
+      link: `/blog/${article.id}/`,
     })),
   })
 }
