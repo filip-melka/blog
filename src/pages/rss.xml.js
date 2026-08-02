4175d8d183583cd3c6a7a69a672a627f28087ebc
Filip Melka <melka.filip@proton.me>
2026-08-02 18:42:39 +0200

    feat: add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function GET(context) {
+  const articles = await getCollection('articles')
+  return rss({
+    title: 'Filip’s Blog',
+    description:
+      'Writing on computer science, math, and programming - explained from first principles.',
+    site: context.site,
+    items: articles.map((article) => ({
+      title: article.data.title,
+      pubDate: article.data.pubDate,
+      description: article.data.description,
+      link: `/blog/articles/${article.id}/`,
+    })),
+  })
+}
